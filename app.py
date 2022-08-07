3ae8b0c3e21ff9da9509a2dee461d6fee22b18a3
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-07 13:59:33 +0100

    login fix test page

--- app.py
+++ app.py
@@ -15,15 +15,13 @@ app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = 'localhost' 
 mysql.init_app(app)
 
-@app.route('/start')
-def start():
-    return render_template("login.html")
-database = {'admin':'admin'}
-
 
 # Default - Show Data
 @app.route("/") 
 def hello(): 
+    return render_template("login.html")
+    database = {'admin':'admin'}
+
   cur = mysql.connection.cursor() #create a connection to the SQL instance
   cur.execute('''SELECT * FROM students''') # execute an SQL statment
   rv = cur.fetchall() #Retreive all rows returend by the SQL statment
