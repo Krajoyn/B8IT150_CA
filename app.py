5e890eb8e0cec4523e879c21c6867a3c6958c2f5
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-07 14:15:56 +0100

    module error fix

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import login_user, login_required
 import pickle
 from flask_mysqldb import MySQL
 from flask_cors import CORS
