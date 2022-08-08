c9c7c52d3d9768e629e5c5f398bd4891342ed41c
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-08 22:04:27 +0100

    test login code

--- auth.py
+++ auth.py
@@ -1,30 +1,25 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-# from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-# from . import db
-# from flask_login import login_user, login_required, logout_user, current_user
+from flask import Flask,request,render_template
+import pickle
+
+auth = Flask(__name__)
 
 
-auth = Blueprint('auth', __name__)
+@auth.route('/')
+def hello_world():
+    return render_template("login.html")
+database={'nachi':'123','james':'aac','karthik':'asdsf'}
 
-
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/form_login',methods=['POST','GET'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+      return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
         else:
-            flash('Email does not exist.', category='error')
+           return render_template('home.html',name=name1)
 
-    return render_template("login.html", user=current_user)
-
-
+if __name__ == '__main__':
+    auth.run()
