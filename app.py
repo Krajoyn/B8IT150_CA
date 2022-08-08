4fc4f628bf45be376fc9ea3c4117f59b96b2c902
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-08 21:36:31 +0100

    login code test

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = 'localhost' 
 mysql.init_app(app)
 
+
+
+
+
+
+
 # Default - Show Data
 @app.route("/") 
 def index(): 
@@ -104,6 +110,39 @@ def update():
   mysql.connection.commit()
   return '{"Result":"Success"}'
 
+
+
+
+
+
+
+
+
+# Login authentication function
+@auth.route("/login",methods=['POST', 'GET'])
+def login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+      session['logged_in'] = True
+      return render_template('main.html')
+
+    else:
+      flash('wrong password!')
+      print("Password incorrect")
+      return render_template('login.html', info="Invalid username and password combination")
+      # return index() 
+
+#     database = {'admin':'admin'}
+#     return render_template("index.html")
+
+#     loginname=request.form['username']
+#     loginpassword=request.form['password']
+#     if loginname not in database:
+#       return render_template('index.html',info='Invalid User')
+#     else:
+#         if database[loginname]!=loginpassword:
+#             return render_template('index.html',info='Invalid Password')
+#         else:
+#            return render_template('main.html',name=loginname)
 
 
 
--- auth.py
+++ auth.py
@@ -1,35 +1,27 @@
-from flask import Flask
-from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for, request, session, abort
-import os
+from flask import Flask,request,render_template
+import pickle
 
-auth = Blueprint('auth', __name__)
+app = Flask(__name__)
 
 
-# Login authentication function
-@auth.route("/login",methods=['POST', 'GET'])
+@app.route('/')
+def hello_world():
+    return render_template("login.html")
+database={'admin':'admin'}
+
+
+
+@app.route('/form_login',methods=['POST','GET'])
 def login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-      session['logged_in'] = True
-      return render_template('main.html')
-
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+      return render_template('login.html',info='Invalid User')
     else:
-      flash('wrong password!')
-      print("Password incorrect")
-      return render_template('login.html', info="Invalid username and password combination")
-      # return index() 
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+           return render_template('home.html',name=name1)
 
-#     database = {'admin':'admin'}
-#     return render_template("index.html")
-
-#     loginname=request.form['username']
-#     loginpassword=request.form['password']
-#     if loginname not in database:
-#       return render_template('index.html',info='Invalid User')
-#     else:
-#         if database[loginname]!=loginpassword:
-#             return render_template('index.html',info='Invalid Password')
-#         else:
-#            return render_template('main.html',name=loginname)
-
-
-
+if __name__ == '__main__':
+    app.run()
