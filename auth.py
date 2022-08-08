642305edde466c5493bc96ce523bb00865b4866e
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-08 21:51:54 +0100

    test login code

--- auth.py
+++ auth.py
@@ -1,24 +1,28 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-  if not session.get('logged_in'):
-    return render_template('login.html')
-  else:
-    return "Hello Boss!"
+auth = Blueprint('auth', __name__)
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-  if request.form['password'] == 'password' and request.form['username'] == 'admin':
-    session['logged_in'] = True
-  else:
-    flash('wrong password!')
-    return home()
 
-if __name__ == "__main__":
-  app.secret_key = os.urandom(12)
-  app.run(debug=True,host='0.0.0.0', port=8081)
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
