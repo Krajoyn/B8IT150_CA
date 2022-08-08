2adf386a1a4cb178baffcc8a2023569dda06c2cf
Krajoyn <104421026+Krajoyn@users.noreply.github.com>
2022-08-08 21:46:31 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -20,5 +20,5 @@ def do_admin_login():
     return home()
 
 if __name__ == "__main__":
-app.secret_key = os.urandom(12)
-app.run(debug=True,host='0.0.0.0', port=4000)
+  app.secret_key = os.urandom(12)
+  app.run(debug=True,host='0.0.0.0', port=4000)
